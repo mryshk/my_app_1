fa86b4fd037bb1463b039810ce37871d60993e69
EC2 Default User <ec2-user@ip-172-31-87-47.ec2.internal>
2021-08-16 07:00:28 +0000

    [update]8/16

--- musicers/app/controllers/musics_controller.rb
+++ musicers/app/controllers/musics_controller.rb
@@ -4,12 +4,18 @@ class MusicsController < ApplicationController
     end
     
     def create
+        @music = Music.new(music_params)
+        @music.user_id = current_user.id
+        @music.save
+        redirect_to music_path(@music)
     end
     
     def show
+        @music = Music.find(params[:id])
     end 
     
     def index
+        @musics = Music.all
     end 
     
     def edit
@@ -18,4 +24,8 @@ class MusicsController < ApplicationController
     def destroy
     end 
    
+   private
+   def music_params
+       params.require(:music).permit(:musicimage,:music_name,:music_caption,:music_url)
+   end
 end
--- musicers/config/routes.rb
+++ musicers/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   
-  resources :users,only:[:show,:index,:edit,:update]
-  resources :musics,only:[:new,:create,:index,:show,:destroy,:edit]
+  resources :users, only: [:new,:show,:index,:edit,:update]
+  resources :musics, only: [:new, :create, :index, :show, :destroy, :edit]
   
 end
