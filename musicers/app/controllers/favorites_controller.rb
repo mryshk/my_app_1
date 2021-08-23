f7d70324ce0961fe53585f6ed69dd7adbc5fb29a
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-08-23 09:17:58 +0000

    [update]AddAjax_ModifyBookShowView

--- musicers/app/controllers/favorites_controller.rb
+++ musicers/app/controllers/favorites_controller.rb
@@ -4,16 +4,11 @@ class FavoritesController < ApplicationController
         @music = Music.find(params[:music_id])
         @favorite = current_user.favorites.new(music_id: @music.id)
         @favorite.save
-        redirect_to music_path(@music)
-
     end
 
     def destroy
         @music = Music.find(params[:music_id])
         @favorite = current_user.favorites.find_by(music_id: @music.id)
         @favorite.destroy
-        redirect_to music_path(@music)
-
-
     end
 end
