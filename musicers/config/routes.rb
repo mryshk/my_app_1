44f9f071045bea71195aa2019dc6417e735d160f
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-09-06 12:33:38 +0000

    [update]AddIndexDESC

--- musicers/config/routes.rb
+++ musicers/config/routes.rb
@@ -9,9 +9,10 @@ Rails.application.routes.draw do
   end
   resources :musics, only: [:new, :create, :index, :show, :destroy, :edit,:update] do
     resource :favorites ,only:[:create,:destroy]
-
     resources :music_comments, only:[:new,:create,:edit,:destroy,:update]
   end
+  get 'index_new_date' => 'musics#index_new_date', as: 'index_new_date'
+  get 'index_rate_desc' => 'musics#index_rate_desc', as: 'index_rate_desc'
 
   resources :groups ,only:[:new,:create,:edit,:update,:show,:index,:destroy] do
     resources :group_users,only:[:create,:destroy,:index]
